788b6d37f1bcc31f818a31310391fcb9e1b54a7b
Muchen He <i@muchen.ca>
2020-05-31 20:44:21 -0700

    Initial Commit

---
+++ MemoryGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MemoryGame
+//
+//  Created by Muchen He on 2020-05-31.
+//  Copyright © 2020 Muchen He. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
